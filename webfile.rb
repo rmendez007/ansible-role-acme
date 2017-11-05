9e34e933796f04a2dd390de011cbe92de83e2a86
spidermonkey <rm@ckr.com>
2017-11-05 10:03:30 -0800

    initial :

---
+++ webfile.rb
@@ -0,0 +1,15 @@
+yo are welcome 
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN 
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+    inet 127.0.0.1/8 scope host lo
+       valid_lft forever preferred_lft forever
+    inet6 ::1/128 scope host 
+       valid_lft forever preferred_lft forever
+2: eno16777736: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP qlen 1000
+    link/ether 00:0c:29:e6:87:9f brd ff:ff:ff:ff:ff:ff
+    inet 192.168.1.110/24 brd 192.168.1.255 scope global dynamic eno16777736
+       valid_lft 80877sec preferred_lft 80877sec
+    inet6 2600:1700:3610:13d0:20c:29ff:fee6:879f/64 scope global noprefixroute dynamic 
+       valid_lft 1209419sec preferred_lft 604619sec
+    inet6 fe80::20c:29ff:fee6:879f/64 scope link 
+       valid_lft forever preferred_lft forever
